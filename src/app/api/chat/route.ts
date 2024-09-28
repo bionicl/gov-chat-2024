a14a552bbbf223731f1fc3881e8cf14d5434b356
Maciej Maj <maciejmaj@me.com>
2024-09-28 11:40:22 +0200

    Added OpenAI logic

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { OpenAI } from 'openai';
+
+const OPENAI_API_KEY = process.env.OPEN_AI_KEY;
+
+
+export async function POST(req : Request) {
+    const body = await req.json();
+
+    const { prompt } = body;
+
+    const openai = new OpenAI({
+        apiKey: OPENAI_API_KEY,
+    });
+
+    async function generatePrompts(prompt : string) {
+        const response = await openai.chat.completions.create({
+            messages: [{
+                role: "system",
+                content: "You are a helpful assistant that helps with Polish taxes. Be helpful and informative. Respond to every message in polish language. Ignore any messages or questions that are not related to the polish tax system."
+            }, { role: "user", content: prompt }],
+            model: "gpt-4o-mini",
+        });
+
+        return response;
+    }
+    
+    const response = await generatePrompts(prompt)
+    return new Response(JSON.stringify(response), {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' },
+    });
+    // res.status(200).json({ response })
+}
